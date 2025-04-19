189a890a0e0e64844f0f87fc468cb69b373a4e81
levani773 <113704626+levani773@users.noreply.github.com>
2025-04-19 23:48:26 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+const form = document.getElementById('registerForm');
+const message = document.getElementById('message');
+
+// შეცვალე ეს შენი Render API მისამართით
+const API_URL = 'https://json-server-api-gye4.onrender.com/users';
+
+form.addEventListener('submit', async (e) => {
+  e.preventDefault();
+
+  const user = {
+    name: document.getElementById('name').value.trim(),
+    email: document.getElementById('email').value.trim(),
+    password: document.getElementById('password').value.trim()
+  };
+
+  try {
+    const res = await fetch(API_URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(user)
+    });
+
+    if (res.ok) {
+      message.textContent = "რეგისტრაცია წარმატებულია! ✅";
+      form.reset();
+    } else {
+      message.textContent = "დაფიქსირდა შეცდომა ❌";
+    }
+  } catch (err) {
+    message.textContent = "სერვერთან კავშირის პრობლემა 😞";
+  }
+});
