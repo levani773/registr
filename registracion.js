adfc46ae4f14a421c10d968df65d9250abcbe640
levani773 <113704626+levani773@users.noreply.github.com>
2025-04-20 16:34:50 +0200

    Update registracion.js

--- registracion.js
+++ registracion.js
@@ -4,14 +4,14 @@ const API_URL = 'https://json-server-api-gye4.onrender.com/users';
 
 form.addEventListener('submit', async (e) => {
   e.preventDefault();
-
-  const user = {
-    first_name: document.getElementById('name').value.trim(),
-    last_name: document.getElementById('last_name').value.trim(),
-    nick_name: document.getElementById('nick_name').value.trim(),
-    email: document.getElementById('email').value.trim(),
-    password: document.getElementById('password').value.trim()
-  };
+const user = {
+  name: document.getElementById('name').value.trim(),
+  last_name: document.getElementById('last_name').value.trim(),
+  nick_name: document.getElementById('nick_name').value.trim(),
+  email: document.getElementById('email').value.trim(),
+  password: document.getElementById('password').value.trim(),
+  role: 'user' // ✅ აქ ვუნიშნავთ რომ ეს არის ჩვეულებრივი მომხმარებელი
+};
 
   try {
     const res = await fetch(API_URL, {
